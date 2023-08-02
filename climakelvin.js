341f5e719c16c931519fadde7be78eaf17810eb9
abreur3beca <123771701+abreur3beca@users.noreply.github.com>
2023-08-02 11:35:33 -0300

    Add files via upload

---
+++ climakelvin.js
@@ -0,0 +1,47 @@
+// atividade 2
+/*criei const (variável para algo que não pode ser mudado) com o nome kelvin
+ e com valor da variável de 293.*/
+
+//atividade 1
+const kelvin = 0;
+
+//atividade 4
+// criei uma variável chamada celsius, que tem o valor da variável kelvin - 273
+
+//atividade 3
+let celsius = kelvin - 273;
+console.log(celsius);
+
+//atividade 6
+//inseri a formúla de fahrenheit em uma variável chamada fahrenheit também
+
+//atividade 5
+let fahrenheit = celsius *(9/5) + 32;
+console.log(fahrenheit);
+
+//atividade 8
+/*imprimi na tela o valor de fahrenheit com o metodo floor
+ para que caso haja uma número decimal ele seja arredondado automaticamente*/ 
+ 
+//atividade 7
+console.log(Math.floor(fahrenheit));
+
+//atividade 9
+console.log(`A temperatura é ${fahrenheit} graus fahrenheit.`);
+
+//atividade 10
+//Com o valor de Kelvin em 293 em fahrenheit é 68
+
+//atividade 11
+//Com o valor de Kelvin em 0 em  fahrenheit é -459.000...
+
+//atividade 12
+let Newton = celsius * 33/100;
+console.log (`A temperatura é ${Math.floor(Newton)} graus newton`);
+
+
+
+
+
+
+
